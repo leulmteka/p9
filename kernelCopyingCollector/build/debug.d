1d39e669d80dd70ae38eabe95071a2ca11a2db91
metalbilly <leulteka@utexas.edu>
2024-04-30 04:48:33 -0500

    final fr

--- kernelCopyingCollector/build/debug.d
+++
@@ -1,4 +0,0 @@
-build/debug.o: debug.cc debug.h \
- /usr/lib/gcc/x86_64-linux-gnu/9/include/stdarg.h io.h stdint.h atomic.h \
- machine.h init.h libk.h smp.h config.h kernel.h shared.h ext2.h ide.h \
- block_io.h
--- kernelCopyingCollector/build/vmm.d
+++
@@ -1,5 +0,0 @@
-build/vmm.o: vmm.cc vmm.h stdint.h machine.h idt.h libk.h \
- /usr/lib/gcc/x86_64-linux-gnu/9/include/stdarg.h io.h blocking_lock.h \
- semaphore.h atomic.h init.h queue.h debug.h threads.h heap.h \
- LinkedList.h smp.h config.h shared.h tss.h ext2.h ide.h block_io.h \
- physmem.h process.h future.h file.h u8250.h
