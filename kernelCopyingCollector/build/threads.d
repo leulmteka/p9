ea7b9e8e9922174ab60f440fd7e776d4138f46fa
metalbilly <leulteka@utexas.edu>
2024-04-30 03:21:45 -0500

    final

--- kernelCopyingCollector/build/threads.d
+++
@@ -1,7 +0,0 @@
-build/threads.o: threads.cc debug.h \
- /usr/lib/gcc/x86_64-linux-gnu/9/include/stdarg.h io.h stdint.h atomic.h \
- machine.h init.h smp.h config.h ext2.h ide.h block_io.h shared.h \
- threads.h queue.h heap.h LinkedList.h vmm.h tss.h process.h \
- blocking_lock.h semaphore.h future.h file.h u8250.h \
- GarbageCollector/CopyingCollector.h GarbageCollector/GarbageCollector.h \
- stdint.h atomic.h debug.h blocking_lock.h threads.h machine.h globals.h
